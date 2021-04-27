9484a2acb9a97fcdedd38d2fd1ff83e9ad5cf732
Samriddhi98 <karmacharyasamriddhi@gmail.com>
2021-04-27 10:22:50 +0545

    sign in with google firebase

---
+++ lib/screens/home.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // detects when user is signed in
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handleSignIn(account);
+    }, onError: (err) {
+      print('Error signing in: $err');
+    });
+    // reauthenticate user when app is opened
+    googleSignIn.signInSilently(suppressErrors: false).then((account) {
+      handleSignIn(account);
+    }).catchError((err) {
+      print('Error signing in: $err');
+    });
+  }
+
+  handleSignIn(GoogleSignInAccount account) {
+    if (account != null) {
+      print('User signed in!: $account');
+      setState(() {
+        isAuth = true;
+      });
+    } else {
+      setState(() {
+        isAuth = false;
+      });
+    }
+  }
+
+  login() {
+    googleSignIn.signIn();
+  }
+
+  logout() {
+    googleSignIn.signOut();
+  }
+
+  Scaffold buildAuthScreen() {
+    // return RaisedButton(
+    //   onPressed: logout,
+    //   child: Text('Logout'),
+    // );
+  }
+
+  Widget buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+              Theme.of(context).accentColor,
+              Theme.of(context).primaryColor
+            ])),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'We Connect',
+              style: TextStyle(
+                fontFamily: 'Signatra',
+                fontSize: 80.0,
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              onTap: () => login(),
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage('assets/images/google_signin_button.png'),
+                  fit: BoxFit.cover,
+                )),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
+  }
+}
