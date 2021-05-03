e1d199aed202f58cf4db42d992b999ed12c7660f
Samriddhi98 <karmacharyasamriddhi@gmail.com>
2021-05-03 17:59:04 +0545

    initial firestore implement

--- lib/screens/timeline.dart
+++ lib/screens/timeline.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:weconnect/widgets/header.dart';
+import 'package:weconnect/widgets/progress.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final usersRef = FirebaseFirestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -7,7 +13,38 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
+  void initState() {
+    getUsers();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  getUsers() async {
+    final QuerySnapshot snapshot = await usersRef
+        .orderBy('postCOunt', descending: true)
+        // .where('postCount', isGreaterThan: 1)
+        //.where('isAdmin', isEqualTo: true)
+        .get();
+
+    snapshot.docs.forEach((DocumentSnapshot doc) {
+      print(doc.data());
+      print(doc.id);
+      print(doc.exists);
+    });
+  }
+
+  getUserById() async {
+    final String id = "M5fUHefPWv2LSnr45AFE";
+    final DocumentSnapshot doc = await usersRef.doc(id).get();
+    print(doc.data());
+    print(doc.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: header(context, isAppTitle: true),
+      body: linearProgress(),
+    );
   }
 }
---
+++ lib/widgets/header.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+AppBar header(context, {bool isAppTitle = false, String titleText}) {
+  return AppBar(
+    title: Text(
+      isAppTitle ? 'WeConnect' : titleText,
+      style: TextStyle(
+          color: Colors.white,
+          fontFamily: isAppTitle ? "Signatra" : "",
+          fontSize: isAppTitle ? 50.0 : 20.0),
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
+}
